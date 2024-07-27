c3593333daec0742ff89d674a32a99464af02c6a
NewSnooker <nu0075533@gmail.com>
2024-07-27 20:44:51 +0700

    fix v1.1

--- app/(back-office)/dashboard/customers/page.jsx
+++ app/(back-office)/dashboard/customers/page.jsx
@@ -1,42 +1,17 @@
 import DataTable from "@/components/data-table-components/DataTable";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { columns } from "./columns";
 import { getData } from "@/lib/getData";
 import Heading from "@/components/backoffice/Heading";
 
 const Customers = async () => {
-  const [customers, setCustomers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getData("customers");
-        setCustomers(data);
-      } catch (err) {
-        setError("Failed to fetch customers.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
+  const customers = await getData("customers");
 
   return (
     <div className="mt-8">
       <Heading title="Customers" />
       <div className="py-8">
-        <DataTable data={customers} columns={columns} filterKeys={["name", "email", "createdAt"]} />
+        <DataTable data={customers} columns={columns} filterKeys={["name","email","createdAt"]} />
       </div>
     </div>
   );
